38bfdd66f8c9cf27d807451873aeae0acbf0ae51
kelsatu <98150024+kelsatu@users.noreply.github.com>
2022-01-21 15:09:21 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Buku Tamu DISKOMINFO ASAHAN</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/diskominfo.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>DISKOMINFO ASAHAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Buku Tamu DISKOMINFO ASAHAN.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data tamu, pengelolaan data instansi, <br>
+                            dan juga pengelolaan laporan data tamu serta asal instansi.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data tamu dan data instansi.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
